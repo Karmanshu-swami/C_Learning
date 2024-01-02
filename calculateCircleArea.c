5be181258087aaba8c1fb7e794e3d6bdf6c36706
karmanshu-swami <swami.karmanshu1398@gmail.com>
2024-01-02 22:35:18 +0530

    2-jan-24 - CalculateSqArea + calculateCircleArea

---
+++ calculateCircleArea.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+
+int main(){
+    int r;
+    float pi = 3.14;
+    printf("Enter the radius of circle : ");
+    scanf("%d", &r);
+    printf("Area of the circle is : %f\n", pi*r*r);
+}
