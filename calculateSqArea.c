4d13b468919e242edd24dbc71513728b4d2149ed
karmanshu-swami <swami.karmanshu1398@gmail.com>
2024-01-02 21:53:09 +0530

    2-jan-24 - CalculateSqArea

---
+++ calculateSqArea.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+// Area of square
+int main(){
+    int aside;
+    printf("Enter the area side : ");
+    scanf("%d", &aside);
+    printf("The area of the square is : %d\n", aside*aside);
+    return 0;
+};
